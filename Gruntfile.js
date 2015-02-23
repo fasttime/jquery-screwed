cecc49c497406b4ecb9ff318491a1cfa5fcd7e84
Francesco Trotta <github@fasttime.org>
2015-02-23 23:00:00 +0100

    New repository

---
+++ Gruntfile.js
@@ -0,0 +1,125 @@
+/* jshint node: true */
+
+'use strict';
+
+module.exports =
+    function (grunt)
+    {
+        // Project configuration.
+        grunt.initConfig(
+            {
+                clean: { default: '*.screwed.js' },
+                jscs:
+                {
+                    default: '*.js',
+                    options:
+                    {
+                        disallowMixedSpacesAndTabs: true,
+                        disallowSpaceAfterObjectKeys: true,
+                        disallowSpaceAfterPrefixUnaryOperators: true,
+                        disallowSpaceBeforePostfixUnaryOperators: true,
+                        disallowSpacesInCallExpression: true,
+                        disallowSpacesInFunctionDeclaration: { beforeOpeningRoundBrace: true },
+                        disallowSpacesInNamedFunctionExpression: { beforeOpeningRoundBrace: true },
+                        disallowSpacesInsideBrackets: true,
+                        disallowSpacesInsideParentheses: true,
+                        disallowTrailingWhitespace: 'ignoreEmptyLines',
+                        disallowYodaConditions: true,
+                        requireBlocksOnNewline: 1,
+                        requireKeywordsOnNewLine:
+                        [
+                            'break',
+                            'case',
+                            'catch',
+                            'continue',
+                            'default',
+                            'do',
+                            'else',
+                            'finally',
+                            'for',
+                            'switch',
+                            'throw',
+                            'try'
+                        ],
+                        requireLineBreakAfterVariableAssignment: true,
+                        requireLineFeedAtFileEnd: true,
+                        requireSpaceAfterBinaryOperators: true,
+                        requireSpaceAfterKeywords: true,
+                        requireSpaceAfterLineComment: true,
+                        requireSpaceBeforeBinaryOperators: true,
+                        requireSpaceBeforeBlockStatements: true,
+                        requireSpaceBeforeKeywords:
+                        [
+                            'delete',
+                            'if',
+                            'in',
+                            'instanceof',
+                            'return',
+                            'while'
+                        ],
+                        requireSpaceBeforeObjectValues: true,
+                        requireSpaceBetweenArguments: true,
+                        requireSpacesInAnonymousFunctionExpression:
+                        {
+                            beforeOpeningRoundBrace: true
+                        },
+                        requireSpacesInConditionalExpression: true,
+                        requireSpacesInForStatement: true,
+                        requireSpacesInFunctionDeclaration: { beforeOpeningCurlyBrace: true },
+                        requireSpacesInFunctionExpression: { beforeOpeningCurlyBrace: true },
+                        requireSpacesInsideObjectBrackets: 'all',
+                        validateIndentation: 4,
+                        validateParameterSeparator: ', '
+                    }
+                },
+                jshint:
+                {
+                    src: '*.js',
+                    options:
+                    {
+                        curly: true,
+                        eqeqeq: true,
+                        immed: true,
+                        latedef: true,
+                        maxlen: 100,
+                        newcap: false,
+                        noarg: true,
+                        noempty: true,
+                        quotmark: true,
+                        singleGroups: true,
+                        strict: true,
+                        trailing: true,
+                        undef: true,
+                        unused: true,
+                        
+                        boss: true,
+                        elision: true,
+                        eqnull: true,
+                        evil: true,
+                        validthis: true,
+                        '-W018': true,
+                    }
+                },
+                shell:
+                {
+                    default:
+                    {
+                        command:
+                            'node node_modules/jscrewit/screw.js ' +
+                            '-w -f SELF ' +
+                            'node_modules/jquery/dist/cdn/jquery-2.1.3.min.js ' +
+                            'jquery-2.1.3.screwed.js'
+                    }
+                }
+            }
+        );
+        
+        // These plugins provide necessary tasks.
+        grunt.loadNpmTasks('grunt-contrib-clean');
+        grunt.loadNpmTasks('grunt-contrib-jshint');
+        grunt.loadNpmTasks('grunt-jscs');
+        grunt.loadNpmTasks('grunt-shell');
+        
+        // Default task.
+        grunt.registerTask('default', ['clean', 'jshint', 'jscs', 'shell']);
+    };
---
+++ README.md
@@ -0,0 +1,61 @@
+jQuery *Screwed*
+================
+
+jQuery JavaScript library made using only six different characters: `[]()!+`
+
+Usage
+-----
+
+You can use jQuery Screwed anywhere in your code like the regular jQuery.
+
+```html
+<script src="jquery-2.1.3.screwed.js"></script>
+```
+
+```html
+<script>
+$(function(){
+    alert("You are using jQuery Screwed!");
+});
+</script>
+```
+
+See the section [Browser Support](#browser-support) for information about supported browsers.
+
+Creation
+--------
+
+jQuery Screwed was created with [JScrewIt](https://github.com/fasttime/JScrewIt).
+Below are the steps to recreate it manually.
+
+* Install [Node.js](http://nodejs.org) if you haven't done so yet.
+* Install JScrewIt: run `npm install -g jscrewit`
+* Download jQuery - current stable version is 2.1.3: http://code.jquery.com/jquery-2.1.3.min.js
+* Replace path names as appropriate and run<br>
+  `node jscrewit -w -f SELF path/to/source/jquery-2.1.3.min.js path/to/dest/jquery-2.1.3.screwed.js`
+
+Browser Support
+---------------
+
+jQuery Screwed supports all browsers also supported by jQuery 2.x, except for those not compatible
+with JScrewIt.
+
+The table below shows browser support of jQuery and jQuery Screwed.
+Differences are marked in bold.
+
+|                   |              jQuery 2.x             |      jQuery Screwed      |
+|-------------------|:-----------------------------------:|:------------------------:|
+| Chrome            |       (Current - 1) or Current      | (Current - 1) or Current |
+| Internet Explorer |                  9+                 |            9+            |
+| Firefox           |       (Current - 1) or Current      | (Current - 1) or Current |
+| Safari            |               **5.1+**              |         **7.0+**         |
+| Opera             | **12.1x**, (Current - 1) or Current | (Current - 1) or Current |
+| iOS               |               **6.1+**              |         **7.1+**         |
+| Android           |            **2.3**, 4.0+            |           4.0+           |
+
+Credits
+-------
+
+jQuery Screwed is based on [jQuery](https://github.com/jquery/jquery).
+jQuery is Copyright jQuery Foundation and other contributors, and licensed under the MIT license.
+
